44d651a840043678a93ebd99e9ab8db63bd960f3
SofianBRh <sofianbrhaiber9h@gmail.com>
2022-07-27 11:09:12 +0200

    Pourvicent

--- index.js
+++ index.js
@@ -1,37 +1,193 @@
-var i = 0;
-var correctCount = 0 ;
 
-generate(0);
+
+var i = 0;
+var CountAnswer = 0 ;
+let AnswerGood = true;
+let counterr =0;
+let score =0;
+let availableQuestion=[];
+const CORREC_BONUS = 100;
+const MAX_QUESTION=23;
+var nom = prompt("Indiquez votre nom");
+var getInput = Array.from(document.getElementsByTagName('input')); // Récupération des inputs 
+//console.log(getInput)
+var getLabel = Array.from(document.getElementsByTagName('span'));
+//console.log(getLabel);
+
+// Fonction MÉLANGE ALÉATOIRE du tableau  
+function shuffleArray(array) {         
+    return array.sort(() => 0.5- Math.random());       
+}
+
+// Mélange le tableau json   
+shuffleArray(jsonData);  
+
 //rempli mon html avec mes donné "Json"
 function generate(index) {
     document.getElementById("question").innerHTML = jsonData[index].question;
-    document.getElementById("optt1").innerHTML = jsonData[index].choice1;
-    document.getElementById("optt2").innerHTML = jsonData[index].choice2;
-    document.getElementById("optt3").innerHTML = jsonData[index].choice3;
-    document.getElementById("optt4").innerHTML = jsonData[index].choice4;
+    
+    document.getElementById("option1").innerHTML = jsonData[index].choix1;
+    document.getElementById("option2").innerHTML = jsonData[index].choix2;
+    document.getElementById("option3").innerHTML = jsonData[index].choix3;
+    document.getElementById("option4").innerHTML = jsonData[index].choix4;
+    
+    document.getElementById("choix1").value = jsonData[index].choix1;
+    document.getElementById("choix2").value = jsonData[index].choix2;
+    document.getElementById("choix3").value = jsonData[index].choix3;
+    document.getElementById("choix4").value = jsonData[index].choix4;
+
+    
 }
 
 
+
+
+
+/* Boucle pour créer un évenement sur les inputs */
+for(index = 0; index < getInput.length; index++){
+    getInput[index].addEventListener('click', Color);
+}
+
+
+/* Fonction qui va colorer les labels des inputs */
+function Color(){
+    
+    getInput.forEach(element => {
+        if(element.checked && element.type == 'radio'){
+            
+            getLabel.forEach(label => {
+
+                if(label.innerHTML == element.value && element.value == jsonData[i].answer){
+                    label.classList.add("correct");
+                }
+                else if(label.innerHTML == element.value && element.value != jsonData[i].answer){
+                    label.classList.add("incorrect");
+                }
+                else{
+                    label.style.backgroundColor = "none";
+                }
+
+            });
+            /* Permet de rendre tous les inputs non cliquables une fois que la réponse est cliquée */
+            document.querySelectorAll('input').forEach(
+                (element)=> {
+                    element.disabled = true;
+                }
+            );
+        }
+    });
+    
+}
+
+
+
+/* Fonction qui va check les réponses */
 function checkAnswer() {
-    if (document.getElementById("choice1").checked && jsonData[i].choice1== jsonData[i].answer) {
-       correctCount++;
+
+    if (document.getElementById("choix1").checked && jsonData[i].choix1== jsonData[i].answer)
+    { document.write.
+        CountAnswer++;
     }
-    if (document.getElementById("choice2").checked && jsonData[i].choice2 == jsonData[i].answer) {
-        correctCount++;
+    if (document.getElementById("choix2").checked && jsonData[i].choix2 == jsonData[i].answer) {
+
+        CountAnswer++;
     }
-    if (document.getElementById("choice3").checked && jsonData[i].choice3 == jsonData[i].answer) {
-        correctCount++;
+    if (document.getElementById("choix3").checked && jsonData[i].choix3 == jsonData[i].answer) {
+    document
+        CountAnswer++;
     }
-    if (document.getElementById("choice4").checked && jsonData[i].choice4 == jsonData[i].answer) {
-        correctCount++;
+    if (document.getElementById("choix4").checked && jsonData[i].choix4 == jsonData[i].answer) {
+        
+        CountAnswer++;
     }
+      localStorage.setItem(nom,CountAnswer)
     // incrementation pour changer de question 
     i++;
+
     if(jsonData.length-1 < i){
         document.write("<body style='background-color:#348322;'>");
-        document.write("<div style='color:#ffffff;font-size:18pt;text-align:center;'>*****Ton score de BG : "+correctCount+"*****</div>");
+        document.write("<div id='salut' style='color:#ffffff;font-size:18pt;text-align:center;'>*****Ton score de BG : "+CountAnswer+"*****</div>");
         document.write("</body>");
+        for (let i = 0; i < 10 ; i++){
+          var ValueB = localStorage.key(i)
+          var ValueA= localStorage.getItem(Valu)
+         
+          div = document.getElementById("salut")
+          var Child = document.createElement("p")
+         
+          Child.style.color = "red";
+          const cNode = document.createTextNode("Nom: " + (Valu)+ " Score: ")
+          const dNode = document.createTextNode(ValueA)
+          
+          Child.appendChild(cNode)
+          Child.appendChild(dNode)
+          div.appendChild(Child)
+          div.appendChild(Child)
+            
     }
+    function compareNombres(a, b) {
+        return a - b;
+        ValueA.sort(compareNombres)
+      }
+  }
+    /* Enlever la classe correct ou incorrect des labels */
+    getLabel.forEach(label => {
+        label.classList.remove("correct");
+        label.classList.remove("incorrect");
+    });
+
+    /* Enlever l'effet "disable" pour les inputs*/
+    document.querySelectorAll('input').forEach(
+        (mesInputs)=> {
+            mesInputs.disabled = false;
+        }
+    );
+
     // Rappel pour generer
-    generate(i);
+    generate(i); 
 }
+
+for(let zIndex = 0; zIndex < allQuestions.length; zIndex++)
+   {
+      // Récupère l imput ( dans html)  de la réponse choisie 
+      inputCheck = document.querySelector('input[name=question' + jsonData+ ']:checked');
+
+      // Si la réponse n'est pas vide
+      if (inputCheck) {
+         var anwser = inputCheck.value; // Réponse choisie
+         var goodAnswer = jsonData[zIndex].reponse; // Bonne réponse
+         var anecdote =  all[zIndex].anecdote; // je recupere les anecdotes
+         var h3 = document.querySelector('#question' + zIndex + ' h3'); // je recupere mon h3 dans  le HTML
+
+         // Si la réponse est bonne
+         if (anwser == goodAnswer) {
+            score++; // +1
+         //affichage bonne réponse en vert
+            inputCheck.parentNode.classList.add("valid")//parenNode permet de selectionner le parent de l'imput (ici div answers ds html)
+           h3.innerText = anecdote; // affichage de l'anecdote
+         // sinon affichage réponse en rouge
+            }else{ 
+            inputCheck.parentNode.classList.add("invalid")
+         }
+
+      }
+
+var anecdote =  allQuestions[zIndex].anecdote; // je recupere les anecdotes
+         var h3 = document.querySelector('#question' + zIndex + ' h3'); // je recupere mon h3 dans  le HTML
+     h3.innerText = anecdote; // affichage de l'anecdote tu mets la derniere ligne dans t a condition vert ou rouge
+// //CONSTANTS
+// //const CORRECT_BONUS = 100;
+// //const MAX_QUESTIONS = 24;
+
+
+
+// //var reponse = document.getElementById('answer').value;
+// //if(reponse == 'answer'){
+  
+//   // document.getElementById("answer").style.color="green";
+// //} else {
+  
+//   // document. getElementById("answer").style.color="red";
+// //}
+//     }
+   }
--- indexx.js
+++ indexx.js
@@ -5,178 +5,178 @@
 var jsonData = [
     {
       "question": "Qui est le premier hokage ?",
-      "choice1": "Hashirama",
-      "choice2" :"Kakashi",
-      "choice3": "Naruto",
-      "choice4": "Minato",
+      "choix1": "Hashirama",
+      "choix2" :"Kakashi",
+      "choix3": "Naruto",
+      "choix4": "Minato",
       "answer":  "Hashirama"
     },
     {
        "question": "Qui a detruit Konoha ?",
-       "choice1": "Pain",
-       "choice2":"Nagato",
-       "choice3": "Konan",
-       "choice4": "Orochimaru",
+       "choix1": "Pain",
+       "choix2":"Nagato",
+       "choix3": "Konan",
+       "choix4": "Orochimaru",
        "answer": "Pain"
      },
      {
        "question": "Comment Naruto appelle t'il jiraya ?",
-       "choice1": "sensei",
-       "choice2":"l'ermite pas net ",
-       "choice3": "Maitre",
-       "choice4": "Bg du 30",
+       "choix1": "sensei",
+       "choix2":"l'ermite pas net ",
+       "choix3": "Maitre",
+       "choix4": "Bg du 30",
        "answer": "l'ermite pas net "
      },
    
      {
        "question": "Quel est le nom de Minato ?",
-       "choice1": "Uzumaki",
-       "choice2":"Uchiha",
-       "choice3": "Namikaze",
-       "choice4": "Kujo",
+       "choix1": "Uzumaki",
+       "choix2":"Uchiha",
+       "choix3": "Namikaze",
+       "choix4": "Kujo",
        "answer": "Namikaze"
      },
    
      {
        "question": "Qui a tué le clan uchiha ?",
-       "choice1": "Naruto",
-       "choice2":"Hinata",
-       "choice3": "Itachi",
-       "choice4": "Sasuke",
+       "choix1": "Naruto",
+       "choix2":"Hinata",
+       "choix3": "Itachi",
+       "choix4": "Sasuke",
        "answer": "Itachi"
      },
    
      {
        "question": "Qui est le frère de Sasuke ? ",
-       "choice1": "NAruto",
-       "choice2":"Minato",
-       "choice3": "Sarutobi",
-       "choice4": "Itachi",
+       "choix1": "NAruto",
+       "choix2":"Minato",
+       "choix3": "Sarutobi",
+       "choix4": "Itachi",
        "answer": "Itachi"
      },
    
      {
        "question": "Qui est la première personne à avoir apprécié Naruto",
-       "choice1": "Hinata",
-       "choice2":"Iruka",
-       "choice3": "Sasuke",
-       "choice4": "Sakura",
+       "choix1": "Hinata",
+       "choix2":"Iruka",
+       "choix3": "Sasuke",
+       "choix4": "Sakura",
        "answer":"Iruka"
      },
    
      {
        "question": "Kakashi est surnommé :",
-       "choice1": "Le ninja copieur",
-       "choice2":"Le ninja au sharingan",
-       "choice3": "Le ninja au bandeu",
-       "choice4": "KA KA SHIIIII",
+       "choix1": "Le ninja copieur",
+       "choix2":"Le ninja au sharingan",
+       "choix3": "Le ninja au bandeu",
+       "choix4": "KA KA SHIIIII",
        "answer":  "Le ninja copieur"
      },
    
      {
        "question": "Quel grand méchant fait partie de la famille de Naruto ?",
-       "choice1": "PAin",
-       "choice2":"Nagato",
-       "choice3": "Karin",
-       "choice4": "Kushina",
+       "choix1": "PAin",
+       "choix2":"Nagato",
+       "choix3": "Karin",
+       "choix4": "Kushina",
        "answer": "Nagato"
      },
    
      {
        "question": "Quelle fille aime Naruto ?",
-       "choice1": "Sakura",
-       "choice2":"Ino",
-       "choice3": "Hinata",
-       "choice4": "Tenten",
+       "choix1": "Sakura",
+       "choix2":"Ino",
+       "choix3": "Hinata",
+       "choix4": "Tenten",
        "answer": "Hinata"
      },
    
      {
        "question": "Dans le film Naruto road to ninja quelle particularité y a-t-il ?",
-       "choice1": "Naruto n'existe pas",
-       "choice2":"Naruto est le Bg du village",
-       "choice3": "Naruto est hokage",
-       "choice4": "Les parent de naruto sont en vie",
+       "choix1": "Naruto n'existe pas",
+       "choix2":"Naruto est le Bg du village",
+       "choix3": "Naruto est hokage",
+       "choix4": "Les parent de naruto sont en vie",
        "answer": "Les parent de naruto sont en vie"
      },
    
      {
        "question": "Quel bras Naruto a-t-il perdu ?",
-       "choice1": "Le gauche",
-       "choice2":"Le droit",
-       "choice3": "Les deux",
-       "choice4": "Aucun",
+       "choix1": "Le gauche",
+       "choix2":"Le droit",
+       "choix3": "Les deux",
+       "choix4": "Aucun",
        "answer": "Les parent de naruto sont en vie"
      },
    
      {
        "question": "Contre qui Naruto a-t-il perdu son bras ?",
-       "choice1": "Itachi",
-       "choice2":"Kakshi",
-       "choice3": "Sasuke",
-       "choice4": "Iwari",
+       "choix1": "Itachi",
+       "choix2":"Kakshi",
+       "choix3": "Sasuke",
+       "choix4": "Iwari",
        "answer": "Sasuke"
      },
      {
        "question": "Quel était le surnom de Kushina ?",
-       "choice1": "La moche",
-       "choice2":"L'etrangere",
-       "choice3": "Le piment rouge sang",
-       "choice4": "La fille aux cheveux rouge ",
+       "choix1": "La moche",
+       "choix2":"L'etrangere",
+       "choix3": "Le piment rouge sang",
+       "choix4": "La fille aux cheveux rouge ",
        "answer": "Le piment rouge sang"
      },
     {
        "question": "Quand Kushina est-elle apparue dans l'esprit de Naruto ?",
-       "choice1": "Quand il dormait",
-       "choice2":"quand il pensait ",
-       "choice3": "quand il a combattu Kiyubi",
-       "choice4": "Quand il mangé",
+       "choix1": "Quand il dormait",
+       "choix2":"quand il pensait ",
+       "choix3": "quand il a combattu Kiyubi",
+       "choix4": "Quand il mangé",
        "answer": "quand il a combattu Kiyubi"
      },{
        "question": "Naruto est la réincarnation de :",
-       "choice1": "Ashura",
-       "choice2":"indra",
-       "choice3": "Minato",
-       "choice4": "Tobirama",
+       "choix1": "Ashura",
+       "choix2":"indra",
+       "choix3": "Minato",
+       "choix4": "Tobirama",
        "answer": "indra"
      },{
        "question": "Quel est le rang de Naruto parmi les hokages ?",
-       "choice1": "cinquieme",
-       "choice2":"sixieme",
-       "choice3": "septieme",
-       "choice4": "huitieme",
+       "choix1": "cinquieme",
+       "choix2":"sixieme",
+       "choix3": "septieme",
+       "choix4": "huitieme",
        "answer": "septieme"
      },{
        "question": "Combien de frères avait Hashirama ?",
-       "choice1": "un",
-       "choice2":"deux",
-       "choice3": "trois",
-       "choice4": "quatre",
+       "choix1": "un",
+       "choix2":"deux",
+       "choix3": "trois",
+       "choix4": "quatre",
        "answer": "deux"
      },
      {
        "question": "Quel âge a Naruto quand il rencontre ses parents ?",
-       "choice1": "treize ans",
-       "choice2":"quatorze ans",
-       "choice3": "quinze ans",
-       "choice4": "seize ans",
+       "choix1": "treize ans",
+       "choix2":"quatorze ans",
+       "choix3": "quinze ans",
+       "choix4": "seize ans",
        "answer": "seize ans"
      },
      {
        "question": " Qui est Akamaru",
-       "choice1": "un chat",
-       "choice2":"le pote a pakun",
-       "choice3": "Un chien ",
-       "choice4": "Le chien de kiba",
+       "choix1": "un chat",
+       "choix2":"le pote a pakun",
+       "choix3": "Un chien ",
+       "choix4": "Le chien de kiba",
        "answer": "Le chien de kiba"
      },
      
      {
        "question": "Qui est mort dans le premier épisode de Naruto ?",
-       "choice1": "le premere hokage",
-       "choice2":"le second hogake",
-       "choice3": "le troisieme hokage",
-       "choice4": "Le quatrieme hokage",
+       "choix1": "le premere hokage",
+       "choix2":"le second hogake",
+       "choix3": "le troisieme hokage",
+       "choix4": "Le quatrieme hokage",
        "answer":  "Le quatrieme hokage"
      }
     ]
